5273367ac5337b18e2381ce6cecdf5f788604533
AntonyBlanco <ablancot@unsa.edu.pe>
2026-04-21 23:51:53 -0500

    Actividad 2: Mostrar titulo subtitulo y boton que muestre un texto

--- app/src/main/java/com/unsadanpbt/todoapp/MainActivity.kt
+++ app/src/main/java/com/unsadanpbt/todoapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.unsadanpbt.todoapp.ui.theme.ToDoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToDoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SegundoEnunciado(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SegundoEnunciado(modifier: Modifier = Modifier) {
+    var mensajeVisible by remember { mutableStateOf(false) }
+    var contador by remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoAppTheme {
-        Greeting("Android")
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "Título de la app",
+            fontSize = 35.sp
+        )
+        Text(
+            text = "Subtítulo de la app",
+            fontSize = 20.sp
+        )
+
+        Button(
+            onClick = {
+                contador++
+                mensajeVisible = !mensajeVisible
+            }
+        ) {
+            Text("Presionado $contador veces")
+        }
+        if (mensajeVisible) {
+            Text(
+                text = "Botón presionado -> Contador: $contador",
+                fontSize = 18.sp
+            )
+        }
     }
 }
